d6e0da2ad9a5e4d0d30af98ee25f4e1ea8458f70
piyush8930 <71957655+piyush8930@users.noreply.github.com>
2026-04-27 13:12:00 +0530

    Parameterize AMI and instance type in main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example-config" {
-  ami = "ami-098e39bafa7e7303d"
-  instance_type = "t3.micro"
+  ami = var.ami
+  instance_type = var.instance_type
 
   tags = {
     Name = "webserver"
